c70dbcc3de8c72c8616d17fc825e4c74e9af91ab
Fabrizio Rizzi <fabrizio@fabriziorizzi.it>
2021-04-21 00:01:34 +0200

    Added header

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="Header">
+    <img class="Logo" src="../assets/logo.svg" />
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    title: String,
+  },
+})
+export default class Header extends Vue {
+  title!: string;
+}
+</script>
+
+<style>
+.Header {
+  display: grid;
+  grid-template-columns: 165px 1fr;
+  align-items: center;
+  border-bottom: 5px solid #333;
+  margin: 20px;
+  padding-bottom: 20px;
+}
+
+.Logo {
+  width: 150px;
+  height: 150px;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,29 +1,35 @@
 <template>
+  <Header :title="'FBI Most Wanted'" />
   <div>Home</div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { Options, Vue } from "vue-class-component";
+import Header from "./Header.vue";
 
 @Options({
-  props: {
-    msg: String
-  }
+  components: {
+    Header,
+  },
 })
-export default class HelloWorld extends Vue {
-  msg!: string;
+export default class Home extends Vue {
 
-  async data() {
-    const results = await fetch('https://server-relay-p7dh73rgd6s7.runkit.sh/', {
-      method: 'POST',
-      headers: {
-        "Content-type": "application/json"
-      },
-      body: JSON.stringify({targetUrl: 'https://api.fbi.gov/wanted/v1/list'})
-    })
+/*   async data() {
+    const results = await fetch(
+      "https://server-relay-p7dh73rgd6s7.runkit.sh/",
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          targetUrl: "https://api.fbi.gov/wanted/v1/list",
+        }),
+      }
+    );
     const parsedResults = await results.json();
     console.log(parsedResults);
     return parsedResults;
-  }
+  } */
 }
 </script>
